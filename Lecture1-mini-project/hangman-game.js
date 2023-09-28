74eb5a57a4afa42c4b3efe5d9657e6ae11ff3dd7
rootdevelopper <rootdevelopper@gmail.com>
2023-09-28 23:58:22 +0700

    Hangman mini project solution

--- Lecture1-mini-project/hangman-game.js
+++ Lecture1-mini-project/hangman-game.js
@@ -1,5 +1,6 @@
 const readline = require('readline');
 const Hangman = require('./hangman');
+const hangmanStates = require('./hangmanStates');
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -15,15 +16,21 @@ function startGame() {
     function playRound() {
       console.log('----------------------------------');
       console.log('Current Status:');
+      console.log('----------------------------------');
+
       console.log(hangman.getGameState().displayWord);
-      console.log('Guesses: ' + hangman.getGameState().guesses);
-      console.log('Attempts Left: ' + hangman.getGameState().attemptsLeft);
+      
+      drawHanging(hangman.getGameState().attemptsLeft);
+      console.log('         Guesses: ' + hangman.getGameState().guesses);
+      console.log('         Attempts Left: ' + hangman.getGameState().attemptsLeft);
 
       rl.question('Guess a letter: ', (letter) => {
         const result = hangman.makeGuess(letter);
         console.log(result);
 
         if (hangman.isGameOver()) {
+
+          drawHanging(0);
           console.log('Game Over!');
           console.log('The word was: ' + hangman.word);
           rl.close();
@@ -35,6 +42,10 @@ function startGame() {
 
     playRound();
   });
+}
+
+function drawHanging(remainingGuesses) {
+  console.log(hangmanStates[remainingGuesses]);
 }
 
 startGame();
